2c0e8c29e9a00a642664d7ae6fc3bd9d2e18b9d0
cjr333 <cjr333@nexon.co.kr>
2018-04-16 23:12:50 +0900

    RNG 완료

--- ch6/src/RNG.scala
+++ ch6/src/RNG.scala
@@ -4,58 +4,46 @@ trait RNG {
 
 object RNG {
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (i, rng1) = rng.nextInt
-    if (i < 0) (math.abs(i + 1), rng1)
-    else (i, rng1)
-  }
-
-  def nonNegativeEven: Rand[Int] = {
-    map(nonNegativeInt)(i => i - i % 2)
+    val (i, rng2) = rng.nextInt
+    if (i < 0) (Math.abs(i + 1), rng2)
+    else (i, rng2)
   }
 
   def double(rng: RNG): (Double, RNG) = {
-    val (i, rng1) = nonNegativeInt(rng)
-    (i.toDouble / (Int.MaxValue + 1), rng1)
+    val (i, rng2) = nonNegativeInt(rng)
+    (i.toDouble / (Int.MaxValue + 1), rng2)
   }
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (i, rng1) = rng.nextInt
-    val (d, rng2) = double(rng1)
-    ((i, d), rng2)
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = double(rng2)
+    ((i, d), rng3)
   }
 
   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val ((i, d), rng1) = intDouble(rng)
-    ((d, i), rng1)
+    val ((i, d), rng2) = intDouble(rng)
+    ((d, i), rng2)
   }
 
   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (d1, rng1) = double(rng)
-    val (d2, rng2) = double(rng1)
-    val (d3, rng3) = double(rng2)
-    ((d1, d2, d3), rng3)
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d1, d2, d3), rng4)
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
     @annotation.tailrec
-    def go(count: Int, tuple1: (List[Int], RNG)): (List[Int], RNG) = {
+    def go(list: List[Int], rng: RNG, count: Int): (List[Int], RNG) = {
       if (count > 0) {
-        val (i, rng1) = tuple1._2.nextInt
-        go(count - 1, (i :: tuple1._1, rng1))
+        val (i, rng2) = rng.nextInt
+        go(i :: list, rng2, count - 1)
+      } else {
+        (list, rng)
       }
-      else tuple1
     }
 
-    go(count, (List(), rng))
-  }
-
-  def intsStackOverflow(count: Int)(rng: RNG): (List[Int], RNG) = {
-    if (count > 0) {
-      val (i, rng1) = rng.nextInt
-      val (list, rng2) = intsStackOverflow(count - 1)(rng1)
-      return (i :: list, rng2)
-    }
-    else (List(), rng)
+    go(List(), rng, count)
   }
 
   type Rand[+A] = RNG => (A, RNG)
@@ -71,8 +59,12 @@ object RNG {
     }
   }
 
-  def double2(rng: RNG): (Double, RNG) = {
-    map(nonNegativeInt)(_.toDouble /  (Int.MaxValue + 1))(rng)
+  def nonNegativeEven: Rand[Int] = {
+    map(nonNegativeInt)(i => i - i % 2)
+  }
+
+  def double2: Rand[Double] = {
+    map(nonNegativeInt)(i => i.toDouble / (Int.MaxValue + 1))
   }
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
@@ -89,27 +81,29 @@ object RNG {
 
   val int: Rand[Int] = _.nextInt
 
-  val randIntDouble: Rand[(Int, Double)] = {
-    both(int, double2)
+  def randIntDouble: Rand[(Int, Double)] = {
+    both(int, double)
   }
 
-  val randDoubleInt: Rand[(Double, Int)] = {
-    both(double2, int)
+  def randDoubleInt: Rand[(Double, Int)] = {
+    both(double, int)
   }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
-    fs.foldRight(unit(List[A]()))((rand, randlist) => map2(rand, randlist)(_ :: _))
+    rng => {
+      fs.foldRight((List[A](), rng))((rand, tuple) => {
+        val (a, rng2) = rand(tuple._2)
+        (a :: tuple._1, rng2)
+      })
+    }
   }
 
-  def ints2(count: Int): Rand[List[Int]] = {
-    sequence(List.fill(count)(int))
+  def sequence2[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List[A]()))((rand, listRand) => map2(rand, listRand)(_ :: _))
   }
 
-  def nonNegativeLessThan(n: Int): Rand[Int] = {
-    flatMap(nonNegativeInt)(i => {
-      val mod = i % n
-      if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
-    })
+  def ints2[A](count: Int)(rng: RNG): (List[Int], RNG) = {
+    sequence2(List.fill(count)(int))(rng)
   }
 
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
@@ -119,7 +113,15 @@ object RNG {
     }
   }
 
-  def map1_2[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    println("nonNegativeLessThan")
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    })
+  }
+
+  def map_2[A, B](s: Rand[A])(f: A => B): Rand[B] = {
     flatMap(s)(a => unit(f(a)))
   }
 
@@ -133,6 +135,15 @@ case class SimpleRNG(seed: Long) extends RNG {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
     val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+case class BeginWithBigInt(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = Int.MaxValue - seed;
+    val nextRNG = BeginWithBigInt(newSeed)
+    val n = (newSeed).toInt
     (n, nextRNG)
   }
 }
--- ch6/src/ex9.scala
+++ ch6/src/ex9.scala
@@ -1,9 +1,9 @@
 object ex9 {
   def main(args: Array[String]): Unit = {
-    println(RNG.map(RNG.unit(5))(_ % 2))
-    println(RNG.map1_2(RNG.unit(5))(_ % 2))
+    println(RNG.map(RNG.unit(5))(_ % 2)(SimpleRNG(1)))
+    println(RNG.map_2(RNG.unit(5))(_ % 2)(SimpleRNG(1)))
 
-    println(RNG.map2(RNG.unit(5), RNG.unit(10))(_ + _))
-    println(RNG.map2_2(RNG.unit(5), RNG.unit(10))(_ + _))
+    println(RNG.map2(RNG.unit(5), RNG.unit(10))(_ + _)(SimpleRNG(1)))
+    println(RNG.map2_2(RNG.unit(5), RNG.unit(10))(_ + _)(SimpleRNG(1)))
   }
 }
